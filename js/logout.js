f3768a360f4277d6efb1dff4cb9315a242de85ca
akbarhabiby <akbarhabiby@icloud.com>
2020-09-25 10:48:22 +0700

    Updated logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,3 +1,8 @@
-localStorage.setItem('textvalue', '') // Set to blank
-alert ('You have been successfully logged out!')
-window.location.href = 'login.html' // Redirect ke login page
+let loggedIn = innerHTML=localStorage.getItem('textvalue')
+if (!loggedIn) {
+    window.location.href = 'login.html'
+} else {
+    localStorage.setItem('textvalue', '') // Set to blank
+    alert ('You have been successfully logged out!')
+    window.location.href = 'login.html' // Redirect ke login page
+}
